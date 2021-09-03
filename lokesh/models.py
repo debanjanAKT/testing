87bf03736156dcae64bb212cb11326c992e4416f
debanjanAKT <90028365+debanjanAKT@users.noreply.github.com>
2021-09-04 01:51:40 +0530

    edit

--- lokesh/models.py
+++ lokesh/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class post(models.Model):
     title =  models.CharField(max_length=250,blank=False,null=True)
-    description = models.CharField(max_length=250,blank=False,null=True)
+    
 
     def __str__(self):
         return self.title
