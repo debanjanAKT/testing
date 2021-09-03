1f65112067f96e1c060fdeb962ec63fb91c29c4c
debanjanAKT <90028365+debanjanAKT@users.noreply.github.com>
2021-09-04 01:43:47 +0530

    p frm

--- testdjango/settings.py
+++ testdjango/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lokesh',
+    'lokesh.apps.LokeshConfig',
 ]
 
 MIDDLEWARE = [
